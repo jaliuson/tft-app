b5f4d58f75a234245156ecfbdefca9666f66967c
Jason Liu <jason.liu29@outlook.com>
2023-04-21 21:59:55 -0400

    First commit

---
+++ src/main/java/com/example/tftapp/TftAppApplication.java
@@ -0,0 +1,45 @@
+package com.example.tftapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+
+@SpringBootApplication
+@RestController
+public class TftAppApplication {
+	private static Map<String, String> jsonOutput = new HashMap<>();
+
+	public static void main(String[] args) {
+		SpringApplication.run(TftAppApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public ResponseEntity<Collection<String>> sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+		//return String.format("Hello %s!", name); //http://localhost:8080/hello?myName=jason
+		jsonOutput.put("name", name);
+		return new ResponseEntity<>(jsonOutput.values(), HttpStatus.OK);
+	}
+
+	@GetMapping("/description")
+	public ResponseEntity<Collection<String>> getProductDescription(){
+		jsonOutput.put("App Description", "Valorant Tracker App!");
+		return new ResponseEntity<>(jsonOutput.values(), HttpStatus.OK);
+	}
+
+	@GetMapping("/match-history")
+	public ResponseEntity<Object> getMatchHistory(){
+		jsonOutput.put("PUUID", "PUUID");
+		jsonOutput.put("Name", "Name");
+		return new ResponseEntity<>(jsonOutput.values(), HttpStatus.OK);
+	}
+}
---
+++ src/test/java/com/example/tftapp/TftAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tftapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
